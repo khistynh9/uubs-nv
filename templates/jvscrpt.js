d41eff11887ad4926704e91cf9ac58f46cca432d
Khisty N Hanafi <51235974+khistynh9@users.noreply.github.com>
2022-10-26 21:22:04 +0700

    update file
    add folder

---
+++ templates/jvscrpt.js
@@ -0,0 +1,56 @@
+$(function () {
+  $("#home").addClass("active");
+  // var $container = $('.cnt');
+  // var $backdrop = $('.backdrop');
+  // var $highlights = $('.highlights');
+  // var $textarea = $('textarea');
+  //pars data
+  // $('#tingkat').on('change', function () {
+  //   const input = $(this).val();
+  //   //   // var text = handleText()
+  //   if (input == '') {
+  //     location.reload();
+  //   } else {
+  //     $('textarea').removeAttr('disabled');
+  // let isi = document.addEventListener('DOMContentLoaded', onReady)
+  // console.log(isi)
+  //     $('#comment').on('keyup', function () {
+  //       var text = $(this).val();
+  //       $.ajax({
+  //         url: '/prosescek',
+  //         data: { input, text },
+  //         type: 'POST',
+  //         dataType: 'json',
+  //         success: function (data) {
+  //           console.log(data);
+  //           var list = [];
+  //           var text = $textarea.val();
+  //           var light = $highlights;
+  //           for (const [key, value] of Object.entries(data)) {
+  //             console.log(`ini ${key}: ${value}`);
+  //             // if (value == '' || value == key) {
+  //             //   console.log('sama');
+  //             // } else {
+  //             //   console.log(`tidak sama ${key}: ${value}`);
+  //             //   // list.push(key);
+  //             // }
+  //           }
+  //           console.log(text)
+  //           // handleInput(light, text, list)
+  //           // $highlights.html(highlightedText);
+
+  //         },
+  //         error: function (error) {
+  //           console.log(error);
+  //         }
+  //       });
+
+  //     });
+
+  //   }
+
+  // });
+
+  //reset
+});
+// onclick = "addCorrect('' + key + '','' + value + '','' + text + '')";
